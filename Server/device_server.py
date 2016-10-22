5ad772302898c1a160a781b9daf3490e78f12a5b
Takuma Shiotsu <taxio.2029@gmail.com>
2016-10-22 11:24:09 +0900

    Modularize device_server

--- Server/device_server.py
+++ Server/device_server.py
@@ -4,26 +4,32 @@
 import ConfigParser
 import SocketServer
 
-inifile = ConfigParser.SafeConfigParser()
-inifile.read('./config.ini')
 
 #UDPサーバー割り込み処理
 class MyUDPHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
         data = self.request[0].strip()
+        user_list = data.split(",")
+        device_user = user_list[0]
+        passed_user = user_list[1:6]
         socket = self.request[1]
-        print "{} wrote:".format(self.client_address[0])
-        print data
-        socket.sendto("ok", self.client_address)
+        print "device user ID:{}".format(device_user)
+        print "passed user ID:{}".format(passed_user)
 
 
-if __name__ == "__main__":
+class DeviceServer:
 
-    #アドレスとポート指定
-    HOST = inifile.get('settings','host')
-    PORT = inifile.getint('settings','port')
-    #サーバー立ち上げ
-    server = SocketServer.UDPServer((HOST, PORT), MyUDPHandler)
-    #割り込みループ
-    server.serve_forever()
+    def __init__(self):
+        self.inifile = ConfigParser.SafeConfigParser()
+        self.inifile.read('./config.ini')
+
+    def run(self):
+        #アドレスとポート指定
+        HOST = self.inifile.get('settings','host')
+        PORT = self.inifile.getint('settings','port')
+        #サーバー立ち上げ
+        server = SocketServer.UDPServer((HOST, PORT), MyUDPHandler)
+        #割り込みループ
+        server.serve_forever()
+
---
+++ Server/server_test_main.py
@@ -0,0 +1,7 @@
+#coding: utf-8
+#!/usr/bin/python
+
+from device_server import DeviceServer
+
+device_server = DeviceServer()
+device_server.run()
